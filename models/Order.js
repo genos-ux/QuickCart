86793eee02365e0354f65b605254ae7a1bfa7783
mrhihihi <gabriel.kyeremateng.gk@gmail.com>
2025-07-12 15:16:02 +0000

    wip: updating order model

---
+++ models/Order.js
@@ -0,0 +1,18 @@
+import { Schema } from "mongoose"
+
+
+const orderSchema = Schema({
+    userId: {type: String, required: true, ref: 'user'},
+    items: [{
+        product: { type: String, required: true, ref: 'product' },
+        quantity: { type: Number, required: true }
+    }],
+    amount: {type: Number, required: true},
+    address: {type: String, ref: 'address', required: true},
+    status: { type: String, required: true, default: 'Order Placed'},
+    date: { type: Number, required: true}
+})
+
+const Order = models.order || mongoose.model('order',orderSchema);
+
+export default Order;
